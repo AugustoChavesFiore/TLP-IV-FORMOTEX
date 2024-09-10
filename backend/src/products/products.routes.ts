8e7cec56491316983a8d2837d45233f5b5b535d4
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2024-09-09 23:22:18 -0300

    Refactorizar rutas de productos para agregar middleware de usuario y administrador

--- backend/src/products/products.routes.ts
+++ backend/src/products/products.routes.ts
@@ -3,6 +3,8 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { ExpressValidatorAdapter } from '../helpers/express-validator';
 import { ProductValidation } from './schemas/products-valitation.schema';
+import { GetUserMiddleware } from '../middlewares/User.middleware';
+import { UserService } from '../users/user.service';
 
 
 export class ProductsRoutes {
@@ -14,12 +16,14 @@ export class ProductsRoutes {
 
         const productsController = new ProductsController(new ProductsService());
         const validation = ExpressValidatorAdapter.validate;
-
-        router.post('/', ProductValidation, validation, productsController.createProduct);
+        const userMiddleware = new GetUserMiddleware(new UserService());
+        const GetUser = userMiddleware.getUser;
+        const VerifyAdmin = userMiddleware.verifyAdmin;
+        router.post('/', [GetUser, VerifyAdmin], ProductValidation, validation, productsController.createProduct);
         router.get('/', productsController.getProducts);
         router.get('/:id', productsController.getProduct);
-        router.put('/:id', ProductValidation, validation, productsController.updateProduct);
-        router.delete('/:id', productsController.deleteProduct);
+        router.put('/:id',  [GetUser, VerifyAdmin], ProductValidation, validation, productsController.updateProduct);
+        router.delete('/:id',  [GetUser, VerifyAdmin], productsController.deleteProduct);
 
 
         return router;
